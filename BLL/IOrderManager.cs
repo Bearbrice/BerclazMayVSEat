53027079e4b0d3df67bc5479ffede9d96ce3d154
Bearbrice <berclazpro@gmail.com>
2019-10-28 10:40:21 +0100

    Order classes and interfaces added everywhere

---
+++ BLL/IOrderManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DTO;
+using DAL;
+
+namespace BLL
+{
+    public interface IOrderManager
+    {
+        IOrderDB OrderDB { get; }
+
+        List<Order> GetOrders();
+
+        Order GetOrder(int id);
+
+        Order AddOrder(Order Order);
+
+        int UpdateOrder(Order Order);
+
+        int DeleteOrder(int id);
+    }
+}
+
