b85388f54b180d027b2574c51945a0869955235c
Bearbrice <berclazpro@gmail.com>
2019-10-28 09:23:01 +0100

    DAL : Restaurant + Staff modifications

--- DAL/IRestaurantDB.cs
+++ DAL/IRestaurantDB.cs
@@ -10,6 +10,16 @@ namespace DAL
     {
         IConfiguration Configuration { get; }
 
-        
+        List<Restaurant> GetRestaurants();
+
+        Restaurant GetResaurant(int id);
+
+        Restaurant AddRestaurant(Restaurant restaurant);
+
+        int UpdateRestaurant(Restaurant restaurant);
+
+        int DeleteRestaurant(int id);
+
+
     }
 }
